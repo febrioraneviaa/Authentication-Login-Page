ca4afec26fd173a4dda1165f685a39f7c5379868
Febriora <febriora@febnevia@gmail.com>
2019-12-13 16:17:52 +0700

    database congfig

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
 if($mysqli === false){
     die("ERROR: Could not connect. " . $mysqli->connect_error);
-}else{
-    echo "Connect DB OK";
+}//else{
+   // echo "Connect DB OK";
 }
 ?>
